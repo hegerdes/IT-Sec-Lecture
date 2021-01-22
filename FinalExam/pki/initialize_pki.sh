21b0e3a49c7cbd0a28f5d48fb8292406b6bf00f0
Henrik Gerdes <hegerdes@uni-osnabrueck.de>
2021-01-22 22:41:43 +0100

    Script now generates all keys/certs

--- FinalExam/pki/initialize_pki.sh
+++ FinalExam/pki/initialize_pki.sh
@@ -5,34 +5,72 @@ NC='\033[0m'
 GRN='\033[0;32m'
 
 CA_FILE_NAME="ca"
+DST_DIR="certificates"
 # Add aditional names for mor clients (seperated by space)
-CLIENTS=(example)
+EXAMPLES=(example)
 
+#Set CWD
+cd "$(dirname "$0")"
+
+#Check openssl
 if ! command -v openssl &> /dev/null
 then
     echo -e "${RED}openssl not be found. Exit${NC}"
     exit
 fi
 
+
+if [ ! -d "$DST_DIR" ]; then
+    mkdir -p "$DST_DIR"
+fi
+
+
 # CA-key
-openssl genrsa -out $CA_FILE_NAME.key 2048
+echo -e "${GRN}Creating CA key${NC}"
+openssl genrsa -out $DST_DIR/$CA_FILE_NAME.key 2048
 # Err check
 if [ $? -ne 0 ]; then
     echo -e "${RED}Error in CA key generation${NC}"
     exit
 fi
 
+# Task 2.1
 # CA-cert
-openssl req -new -key $CA_FILE_NAME.key -x509 -days 3650 -batch -subj "/CN=authority-969272/C=DE/ST=LowerSaxony/L=Osnabrueck/O=UNI/OU=StudentSigner/emailAddress=hegerdes@uos.de" -out $CA_FILE_NAME.pem
+openssl req -new -key $DST_DIR/$CA_FILE_NAME.key -x509 -days 3650 -batch -subj "/CN=authority-969272/C=DE/ST=LowerSaxony/L=Osnabrueck/O=UNI/OU=StudentSigner/emailAddress=hegerdes@uos.de" -out $DST_DIR/$CA_FILE_NAME.pem
 # Err check
 if [ $? -ne 0 ]; then
     echo -e"${RED}Error in CA crt generation${NC}"
     exit
 fi
 
-for client in ${CLIENTS[@]}; do
+for example in ${EXAMPLES[@]}; do
     # Example key and sign-request
-    openssl req -new -newkey rsa:2048 -nodes -keyout $client.key -days 3650 -batch -subj "/CN=user-969272/C=DE/ST=LowerSaxony/L=Osnabrueck/O=UNI/OU=Student/emailAddress=hegerdes@uos.de" -out $client.csr
+    echo -e "${GRN}Creating ${example} key and sign request ${NC}"
+    openssl req -new -newkey rsa:2048 -nodes -keyout $DST_DIR/$example.key -days 3650 -batch -subj "/CN=user-969272/C=DE/ST=LowerSaxony/L=Osnabrueck/O=UNI/OU=Student/emailAddress=hegerdes@uos.de" -out $DST_DIR/$example.csr
+
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}Error in example key/csr generation${NC}"
+        exit
+    fi
+
+    # Sign with CA
+    echo -e "${GRN}Signing ${example} with CA ${NC}"
+    openssl x509 -req -days 365 -in $DST_DIR/$example.csr -CA $DST_DIR/$CA_FILE_NAME.pem -CAkey $DST_DIR/$CA_FILE_NAME.key -CAcreateserial -set_serial 01 -out $DST_DIR/$example.pem
+
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}Error at client sign${NC}"
+        exit
+    fi
+done
+
+
+
+# Task 2.2 & Task 2.3
+CLIENTS=(server client1 client2)
+
+for client in ${CLIENTS[@]}; do
+    echo -e "${GRN}Creating ${client} key and sign request ${NC}"
+    openssl req -new -newkey rsa:2048 -nodes -keyout $DST_DIR/$client.key -days 365 -batch -out $DST_DIR/$client.csr -config openssl.conf -subj "/CN=${client}-969272/C=DE/ST=LowerSaxony/L=Osnabrueck/O=UNI/OU=${client}/emailAddress=${client}@uos.de"
 
     if [ $? -ne 0 ]; then
         echo -e "${RED}Error in client key/csr generation${NC}"
@@ -40,7 +78,8 @@ for client in ${CLIENTS[@]}; do
     fi
 
     # Sign with CA
-    openssl x509 -req -days 365 -in $client.csr -CA $CA_FILE_NAME.pem -CAkey $CA_FILE_NAME.key -CAcreateserial -set_serial 01 -out $client.pem
+    echo -e "${GRN}Signing ${client} with CA ${NC}"
+    openssl x509 -req -days 365 -in $DST_DIR/$client.csr -CA $DST_DIR/$CA_FILE_NAME.pem -CAkey $DST_DIR/$CA_FILE_NAME.key -CAcreateserial -set_serial 01 -out $DST_DIR/$client.pem
 
     if [ $? -ne 0 ]; then
         echo -e "${RED}Error at client sign${NC}"
@@ -49,4 +88,3 @@ for client in ${CLIENTS[@]}; do
 done
 
 echo -e "${GRN}Everything generated succsesfully${NC}"
-
