fcfd5a27466d7be5588a5602651dacd47fd5eec8
Henrik Gerdes <hegerdes@uni-osnabrueck.de>
2021-01-25 04:04:14 +0100

    Renamed connection context

--- FinalExam/proxy_server.py
+++ FinalExam/proxy_server.py
@@ -11,8 +11,6 @@ from helper.ProxyParser import constants as CONST
 from helper.ProxyParser import ParseACL
 from BaseProxy import Tunnel
 
-example_request = 'GET / HTTP/1.1\r\nHost: localhost:8000\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nSec-Fetch-Site: none\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-User: ?1\r\nSec-Fetch-Dest: document\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.9,de;q=0.8\r\n\r\n'
-
 
 def checkACL(conf, cert):
     if not conf.acl:
@@ -34,31 +32,43 @@ def checkEnd(data):
             return True
     return False
 
+def socks_handler(context):
+    data = context.request.recv(CONST.REV_BUFFER)
+    if len(data) < 9:
+        print('Error')
+        return
+    vn, cn, port, ip1, ip2, ip3, ip4 = struct.unpack('bbhbbbb', data[:8])
+    print(struct.unpack('bbhbbbb', data[:8]))
+    pass
 
-def proxy_serv_handler(self):
-    conf = self.server.conf
+
+def proxy_serv_handler(context):
+    conf = context.server.conf
+
+    if conf.socks:
+        return socks_handler(context)
 
     use_ssl = True
     try:
-        client_cert = self.request.getpeercert()
+        client_cert = context.request.getpeercert()
         if client_cert:
             print(CL.GRY + 'ClientCertSubject' + CL.NC,
-                  client_cert['subject'], self.request.version())
+                  client_cert['subject'], context.request.version())
 
         # User not in ACL
         if 'ca' in conf.ssl and not checkACL(conf, client_cert):
             print(CL.RED + 'A unautorised user tried to use the Proxy!' + CL.NC)
             if client_cert:
                 print('His cert:\n', client_cert['subject'])
-            self.request.send(struct.pack('5sb', CONST.PROT_ID,
+            context.request.send(struct.pack('5sb', CONST.PROT_ID,
                                           CONST.BIT_FLAG_MASK['ACL_FAIL_FLAG']))
-            self.request.close()
+            context.request.close()
             return
 
     except AttributeError:
         use_ssl = False
 
-    data = self.request.recv(CONST.REV_BUFFER)
+    data = context.request.recv(CONST.REV_BUFFER)
     fixed_header = data[:CONST.FIXED_HEADER]
     dst_data = data[CONST.FIXED_HEADER:]
     prot, conn_flag = struct.unpack('5sb', fixed_header)
@@ -66,9 +76,9 @@ def proxy_serv_handler(self):
     # Check proxy protocol
     if conn_flag != CONST.BIT_FLAG_MASK['CON_Flag'] or prot != CONST.PROT_ID:
         print(CL.RED + 'Invalid request. Closing...' + CL.NC)
-        self.request.sendall(struct.pack(
+        context.request.sendall(struct.pack(
             '5sb', CONST.PROT_ID, CONST.BIT_FLAG_MASK['PROT_ERR_Flag']))
-        self.request.close()
+        context.request.close()
         return
 
     # DST info
@@ -79,16 +89,16 @@ def proxy_serv_handler(self):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as dst_sock:
             dst_sock.connect((url, port))
-            print(CL.BLU + 'client ' + str(self.request.getpeername()) +
+            print(CL.BLU + 'client ' + str(context.request.getpeername()) +
                   ' requested ' + str((url, port)) + '. Connection to dst: OK' + CL.NC)
 
-            self.request.sendall(struct.pack('5sb', CONST.PROT_ID,
+            context.request.sendall(struct.pack('5sb', CONST.PROT_ID,
                                              CONST.BIT_FLAG_MASK['CON_ACK_Flag']))
 
             while True:
-                r, w, x = select.select([self.request, dst_sock], [], [])
-                if self.request in r:
-                    data = self.request.recv(CONST.REV_BUFFER)
+                r, w, x = select.select([context.request, dst_sock], [], [])
+                if context.request in r:
+                    data = context.request.recv(CONST.REV_BUFFER)
                     if checkEnd(data):
                         break
                     if len(data) == 0:
@@ -97,21 +107,21 @@ def proxy_serv_handler(self):
                 if dst_sock in r:
                     data = dst_sock.recv(CONST.REV_BUFFER)
                     if len(data) != 0:
-                        self.request.send(data)
+                        context.request.send(data)
 
     except socket.error as e:
         print((CL.RED + 'Request from {}: Unable to connect to destination: ({},{})' +
-               CL.NC).format(self.request.getpeername(), url, port))
-        self.request.sendall(struct.pack(
+               CL.NC).format(context.request.getpeername(), url, port))
+        context.request.sendall(struct.pack(
             '5sb', CONST.PROT_ID, CONST.BIT_FLAG_MASK['DST_FAIL_Flag']))
-        self.request.close()
+        context.request.close()
         return
 
-    print(CL.GRY + 'Connection closed from', self.request.getpeername(), CL.NC)
-    self.request.close
+    print(CL.GRY + 'Connection closed from', context.request.getpeername(), CL.NC)
+    context.request.close
 
 
-def client(ip, port, message, setup_ssl=False):
+def TestClient(ip, port, message, setup_ssl=False):
     if setup_ssl:
         ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
         ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
@@ -134,7 +144,7 @@ def client(ip, port, message, setup_ssl=False):
         dst_payload = struct.pack(
             'IH' + str(len(dst_host)) + 's', len(dst_host), dst_port, dst_host.encode())
         app_payload = struct.pack(
-            'L' + str(len(example_request)) + 's', len(example_request), example_request.encode())
+            'L' + str(len(CONST.EXAMPLE_REQ)) + 's', len(CONST.EXAMPLE_REQ), CONST.EXAMPLE_REQ.encode())
 
         if setup_ssl:
             print('ServerCert', sock.getpeercert())
@@ -179,13 +189,13 @@ if __name__ == "__main__":
     except PermissionError as e:
         print(CL.RED + 'Permission error. Action not allowed. ErrMSG: ' + str(e) + CL.NC)
         exit(1)
-    # except OSError as e:
-    #     print(
-    #         CL.RED + 'OSError. Probably the port is already used. ErrMSG: ' + str(e) + CL.NC)
-    #     exit(1)
+    except OSError as e:
+        print(
+            CL.RED + 'OSError. Probably the port is already used. ErrMSG: ' + str(e) + CL.NC)
+        exit(1)
 
     # Testing
     ip, port = ('127.0.0.1', 7622)
-    client(ip, port, "Hello World 1", False)
-    # client(ip, port, "Hello World 2")
-    # client(ip, port, "Hello World 3")
+    # TestClient(ip, port, "Hello World 1", False)
+    # TestClient(ip, port, "Hello World 2")
+    # TestClient(ip, port, "Hello World 3")
