cef32a64f760fec283a049e3f2b85727657b6ff9
Henrik Gerdes <hegerdes@uni-osnabrueck.de>
2021-01-27 21:58:16 +0100

    Bind handling in SOCKS

--- FinalExam/MySOCKS.py
+++ FinalExam/MySOCKS.py
@@ -10,23 +10,27 @@ from helper.ProxyParser import color as CL
 
 def socks_handler(context):
     data = context.request.recv(CONST.REV_BUFFER)
-    if len(data) < 9:
-        print('Error')
-        return
-
-    print(struct.unpack('!HBBBB', data[2:8]))
-    vn, cn = struct.unpack('!BB', data[:2])
+    protInfo1 = getProtInfo(data)
+    vn, cn, port, ip1, ip2, ip3, ip4 = protInfo1
     assert vn == CONST.SOCKS_VERSION
 
-    port, ip1, ip2, ip3, ip4 = struct.unpack('!HBBBB', data[2:8])
-
-    if cn != 1:
+    if cn != 1 and vn != 2:
         # Bind is handeld by the SocketServer TCP
-        print(CL.RED + 'Unsuported SOCKS cn')
+        print(CL.RED + 'Unsuported SOCKS request' + CL.NC)
         context.request.send(struct.pack(
-                '!BBHBBBB', 0, 91, port, ip1, ip2, ip3, ip4))
+            '!BBHBBBB', 0, 91, port, ip1, ip2, ip3, ip4))
         return
 
+    # requestet a bind. But we are alrady have a bind listinimg SOCKS server.
+    # So send this socket address
+    if cn == 2:
+        context.request.send(struct.pack('!BBHBBBB', 0, 90, 0, 0, 0, 0, 0))
+        protInfo2 = getProtInfo(context.request.recv(CONST.REV_BUFFER))
+
+        if not equalDST(protInfo1, protInfo2):
+            context.request.send(struct.pack(
+                '!BBHBBBB', 0, 91, port, ip1, ip2, ip3, ip4))
+            return
 
     if isHost((ip1, ip2, ip3, ip4)):
         length = len(data) - 9 - 1
@@ -34,7 +38,8 @@ def socks_handler(context):
                                 data[9: len(data) - 1])[0]
     else:
         address = socket.inet_ntoa(data[4:8])
-    print((CL.BLU + '{} requestet {}:{}' + CL.NC).format(context.request.getpeername(), address, port) )
+    print((CL.BLU + '{} requestet {}:{}' +
+           CL.NC).format(context.request.getpeername(), address, port))
 
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as dst_socket:
@@ -42,14 +47,13 @@ def socks_handler(context):
             dst_socket.connect((address, port))
             tmp = list()
             [tmp.append(int(ip_int))
-            for ip_int in dst_socket.getpeername()[0].split('.')]
+             for ip_int in dst_socket.getpeername()[0].split('.')]
             ip1, ip2, ip3, ip4 = tmp
             context.request.send(struct.pack(
                 '!BBHBBBB', 0, 90, port, ip1, ip2, ip3, ip4))
 
             while True:
                 r, w, x = select.select([context.request, dst_socket], [], [])
-                print(data)
                 if context.request in r:
                     data = context.request.recv(CONST.REV_BUFFER)
                     if len(data) == 0:
@@ -62,8 +66,24 @@ def socks_handler(context):
                     context.request.send(data)
     except socket.error as e:
         context.request.send(struct.pack(
-                '!BBHBBBB', 0, 92, port, ip1, ip2, ip3, ip4))
-        print(CL.RED + 'Error in the dst connection!' + CL.NC + '\nErrMSG: ' +str(e))
+            '!BBHBBBB', 0, 92, port, ip1, ip2, ip3, ip4))
+        print(CL.RED + 'Error in the dst connection!' +
+              CL.NC + '\nErrMSG: ' + str(e))
+
+def getProtInfo(data):
+    if len(data) < 9:
+        print('Error')
+        return None
+
+    return struct.unpack('!BBHBBBB', data[:8])
+
+
+def equalDST(info1, info2):
+    isEqual = True
+    for i in range(2, 7):
+        if info1[i] != info2[i]:
+            isEqual = False
+    return isEqual
 
 
 def TestSocks(host, port, destination=("icanhazip.com", 80)):
