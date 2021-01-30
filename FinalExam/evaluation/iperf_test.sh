4f85da6e931b079f36bb076ef0549c0643b60660
Henrik Gerdes <hegerdes@uni-osnabrueck.de>
2021-01-30 05:41:38 +0100

    Confs for both testervers

--- FinalExam/evaluation/iperf_test.sh
+++ FinalExam/evaluation/iperf_test.sh
@@ -5,16 +5,17 @@ RED='\033[0;31m'
 NC='\033[0m'
 GRN='\033[0;32m'
 
-ITERATIONS=1
-LOG_FILE='iperf_bones.log'
+ITERATIONS=5
+PACKAGE_SIZE=32MB
+SERVER="diggory"
+LOG_FILE="iperf_${SERVER}.log"
 
 #Set CWD
 cd "$(dirname "$0")"
-PACKAGE_SIZE=12MB
 
 for iteration in $(seq 1 $ITERATIONS); do
     echo "NoProxy" >> $LOG_FILE
-    iperf -c bones.informatik.uni-osnabrueck.de -i 0.5 -l 8KB -p 2622 -n $PACKAGE_SIZE >> $LOG_FILE
+    iperf -c ${SERVER}.informatik.uni-osnabrueck.de -i 0.5 -l 8KB -p 2622 -n $PACKAGE_SIZE >> $LOG_FILE
 
     echo "ProxyNoSSL" >> $LOG_FILE
     iperf -c 127.0.0.1 -i 0.5 -l 8KB -p 8000 -n $PACKAGE_SIZE >> $LOG_FILE
